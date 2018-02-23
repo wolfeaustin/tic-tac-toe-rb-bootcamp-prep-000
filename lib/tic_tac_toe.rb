4d995f99b1c99b04d3ad7917de5ab406838a0382
Austin Wolfe <wolfe_austin@yahoo.com>
2018-02-23 13:21:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,61 @@
+def input_to_index(user_input)
+  (user_input - 1).to_s 
+end 
 
+def move(board, index, player = "X")
+  
+  
+
+end 
+
+def position_taken?(board, index)
+  
+  
+  
+end 
+
+def valid_move(board, index)
+  
+  if board[index] == " "
+    return false
+  else 
+    return TRUE
+end 
+
+def turn ()
+  
+  
+  
+  
+end 
+
+def turn_count()
+  
+  
+end 
+
+
+def current_player(board)
+  
+  
+  
+end 
+
+
+def won?(board)
+  
+  
+  
+  
+  
+end 
+
+
+def full?(board)
+  
+  
+  
+  
+end 
+
+def 
